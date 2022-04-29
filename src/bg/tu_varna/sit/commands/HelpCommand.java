95a24cd97330575a6b421863bb1a2501345eb599
danielyovchev <0885392206dD>
2022-04-29 19:33:15 +0300

    helper

--- src/bg/tu_varna/sit/commands/HelpCommand.java
+++ src/bg/tu_varna/sit/commands/HelpCommand.java
@@ -3,6 +3,8 @@ package bg.tu_varna.sit.commands;
 public class HelpCommand implements Command{
     @Override
     public void execute(Object[] args) {
-
+        System.out.println("The following commands are supported:\n"+"open <file> opens <file>\n"+"close    closes currently opened file \n"+
+                "save   saves the currently open file\n"+"saveas<file>  saves the currently open file in <file>\n"+
+                "help   prints this information\n"+"exit    exits the program");
     }
 }
