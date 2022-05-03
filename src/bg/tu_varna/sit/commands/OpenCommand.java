b2f93911293d5e9e3f9371585446d9ca2846985e
danielyovchev <0885392206dD>
2022-05-03 22:50:54 +0300

    adding

--- src/bg/tu_varna/sit/commands/OpenCommand.java
+++ src/bg/tu_varna/sit/commands/OpenCommand.java
@@ -2,8 +2,11 @@ package bg.tu_varna.sit.commands;
 
 public class OpenCommand implements Command{
     public static boolean openedFile=false;
+    public static String path;
+    public static String filename;
     @Override
     public void execute(Object[] args) {
 
+        System.out.println("Successfully opened " + filename);
     }
 }
--- src/bg/tu_varna/sit/parser/SVGToObject.java
+++ src/bg/tu_varna/sit/parser/SVGToObject.java
@@ -1,4 +1,5 @@
 package bg.tu_varna.sit.parser;
 
 public class SVGToObject {
+    
 }
